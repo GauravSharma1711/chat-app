d57269062d0d883d99cf7c941713f9db3a5250d9
Gaurav Sharma <gau1711sha@gmail.com>
2024-11-02 19:10:49 +0530

    Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,9 @@
+import path from "path";
+
 import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
+
 
 import authRoutes from './routes/auth.routes.js'
 import messageRoutes from './routes/message.routes.js'
@@ -12,7 +15,11 @@ import cookieParser from 'cookie-parser';
 import { app , server} from './socket/socket.js';
 
 
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+
+const __dirname = path.resolve();
+
+
 
 app.use(express.json());
 app.use(cookieParser());
@@ -20,6 +27,13 @@ app.use(cookieParser());
 app.use('/api/auth', authRoutes)
 app.use('/api/messages',messageRoutes)
 app.use('/api/users',userRoutes)
+
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend" , "dist" , "index.html"));
+});
+
 
 
 
